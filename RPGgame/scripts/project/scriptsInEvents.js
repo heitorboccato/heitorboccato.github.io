0b28765fd154997c34fff96faff2b2182abd939e
heitorboccato <42947486+heitorboccato@users.noreply.github.com>
2023-06-02 20:18:43 -0300

    Add files via upload

---
+++ RPGgame/scripts/project/scriptsInEvents.js
@@ -0,0 +1,210 @@
+
+
+
+const scriptsInEvents = {
+
+	async EventSheet1_Event13_Act1(runtime, localVars)
+	{
+		var style = document.createElement('style');
+		style.innerHTML = `
+		    .hp_menos {
+		        font-size:18px;
+				  font-family:Arial;
+				  width:55px;
+				  height:55px;
+				  border-width:1px;
+				  color:#fff;
+				  border-color:#350D11;
+				  font-weight:bold;
+				  border-top-left-radius:28px;
+				  border-top-right-radius:28px;
+				  border-bottom-left-radius:28px;
+				  border-bottom-right-radius:28px;
+				  text-shadow: 1px 1px 0px #2f6627;
+				  background:#FF2700;
+		    }
+		`;
+		
+		document.head.appendChild(style);
+	},
+
+	async EventSheet1_Event14_Act1(runtime, localVars)
+	{
+		var style = document.createElement('style');
+		style.innerHTML = `
+		    .hp_mais {
+		        font-size:18px;
+				  font-family:Arial;
+				  width:55px;
+				  height:55px;
+				  border-width:1px;
+				  color:#fff;
+				  border-color:#18ab29;
+				  font-weight:bold;
+				  border-top-left-radius:28px;
+				  border-top-right-radius:28px;
+				  border-bottom-left-radius:28px;
+				  border-bottom-right-radius:28px;
+				  text-shadow: 1px 1px 0px #2f6627;
+				  background:#44c767;
+		    }
+		`;
+		
+		document.head.appendChild(style);
+	},
+
+	async EventSheet1_Event15_Act1(runtime, localVars)
+	{
+		var style = document.createElement('style');
+		style.innerHTML = `
+		    .atq_menos {
+		        font-size:18px;
+				  font-family:Arial;
+				  width:55px;
+				  height:55px;
+				  border-width:1px;
+				  color:#fff;
+				  border-color:#350D11;
+				  font-weight:bold;
+				  border-top-left-radius:28px;
+				  border-top-right-radius:28px;
+				  border-bottom-left-radius:28px;
+				  border-bottom-right-radius:28px;
+				  text-shadow: 1px 1px 0px #2f6627;
+				  background:#FF2700;
+		    }
+		`;
+		
+		document.head.appendChild(style);
+	},
+
+	async EventSheet1_Event16_Act1(runtime, localVars)
+	{
+		var style = document.createElement('style');
+		style.innerHTML = `
+		    .atq_mais {
+		        font-size:18px;
+				  font-family:Arial;
+				  width:55px;
+				  height:55px;
+				  border-width:1px;
+				  color:#fff;
+				  border-color:#18ab29;
+				  font-weight:bold;
+				  border-top-left-radius:28px;
+				  border-top-right-radius:28px;
+				  border-bottom-left-radius:28px;
+				  border-bottom-right-radius:28px;
+				  text-shadow: 1px 1px 0px #2f6627;
+				  background:#44c767;
+		    }
+		`;
+		
+		document.head.appendChild(style);
+	},
+
+	async EventSheet1_Event17_Act1(runtime, localVars)
+	{
+		var style = document.createElement('style');
+		style.innerHTML = `
+		    .def_mais {
+		        font-size:18px;
+				  font-family:Arial;
+				  width:55px;
+				  height:55px;
+				  border-width:1px;
+				  color:#fff;
+				  border-color:#18ab29;
+				  font-weight:bold;
+				  border-top-left-radius:28px;
+				  border-top-right-radius:28px;
+				  border-bottom-left-radius:28px;
+				  border-bottom-right-radius:28px;
+				  text-shadow: 1px 1px 0px #2f6627;
+				  background:#44c767;
+		    }
+		`;
+		
+		document.head.appendChild(style);
+	},
+
+	async EventSheet1_Event18_Act1(runtime, localVars)
+	{
+		var style = document.createElement('style');
+		style.innerHTML = `
+		    .def_menos {
+		        font-size:18px;
+				  font-family:Arial;
+				  width:55px;
+				  height:55px;
+				  border-width:1px;
+				  color:#fff;
+				  border-color:#350D11;
+				  font-weight:bold;
+				  border-top-left-radius:28px;
+				  border-top-right-radius:28px;
+				  border-bottom-left-radius:28px;
+				  border-bottom-right-radius:28px;
+				  text-shadow: 1px 1px 0px #2f6627;
+				  background:#FF2700;
+		    }
+		`;
+		
+		document.head.appendChild(style);
+	},
+
+	async EventSheet1_Event19_Act1(runtime, localVars)
+	{
+		var style = document.createElement('style');
+		style.innerHTML = `
+		    .agi_menos {
+		        font-size:30px;
+				  font-family:Arial;
+				  width:55px;
+				  height:55px;
+				  border-width:1px;
+				  color:#fff;
+				  border-color:#350D11;
+				  font-weight:bold;
+				  font-color: black;
+				  border-top-left-radius:28px;
+				  border-top-right-radius:28px;
+				  border-bottom-left-radius:28px;
+				  border-bottom-right-radius:28px;
+				  text-shadow: 1px 1px 0px #2f6627;
+				  background:#FF2700;
+		    }
+		`;
+		
+		document.head.appendChild(style);
+	},
+
+	async EventSheet1_Event20_Act1(runtime, localVars)
+	{
+		var style = document.createElement('style');
+		style.innerHTML = `
+		    .agi_mais {
+		        font-size:18px;
+				  font-family:Arial;
+				  width:55px;
+				  height:55px;
+				  border-width:1px;
+				  color:#fff;
+				  border-color:#18ab29;
+				  font-weight:bold;
+				  border-top-left-radius:28px;
+				  border-top-right-radius:28px;
+				  border-bottom-left-radius:28px;
+				  border-bottom-right-radius:28px;
+				  text-shadow: 1px 1px 0px #2f6627;
+				  background:#44c767;
+		    }
+		`;
+		
+		document.head.appendChild(style);
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
